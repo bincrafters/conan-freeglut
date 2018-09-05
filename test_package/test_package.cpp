0acc9047af3e46271992c53c01f2f487f39614e3
Croydon <cr0ydon@outlook.com>
2018-09-05 09:29:59 +0200

    Init test_package

---
+++ test_package/test_package.cpp
@@ -0,0 +1,17 @@
+#include "GL/freeglut.h"
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(600, 600);
+    glutCreateWindow("Test package");
+
+    glClearColor(0.5, 0.5, 0.5, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glLoadIdentity();
+    glutSolidCube(0.1);
+
+    return 0;
+}
