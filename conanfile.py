bfbb2b46ef05a07463d8b4a31ce5b35c13218a6a
bincrafters-user <bincrafters@gmail.com>
2019-10-16 04:40:20 +0000

    Update Conan conventions [build=outdated]
    Automatically created by bincrafters-conventions 0.13.0

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from conans import ConanFile, CMake, tools
 import os
 
@@ -53,6 +51,7 @@ class freeglutConan(ConanFile):
 
     def configure(self):
         del self.settings.compiler.libcxx
+        del self.settings.compiler.cppstd
 
     def source(self):
         archive_url = "{}/archive/FG_{}.tar.gz".format(self.homepage, self.version.replace(".", "_"))
