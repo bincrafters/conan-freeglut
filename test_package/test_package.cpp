a90fb89660794447c55b4fa26e315ee9c1c6d4f2
Eric Lemanissier <eric.lemanissier@gmail.com>
2020-01-13 20:41:13 +0100

    fix test recipe

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -9,10 +9,35 @@ void error_handler(const char *fmt, va_list ap) {
     std::cout << fmt << std::endl;
 }
 
+void renderScene(void) {
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glBegin(GL_TRIANGLES);
+		glVertex3f(-0.5,-0.5,0.0);
+		glVertex3f(0.5,0.0,0.0);
+		glVertex3f(0.0,0.5,0.0);
+	glEnd();
+
+        glutSwapBuffers();
+}
+
 int main(int argc, char **argv) {
     glutInitErrorFunc(error_handler);
+    glutInit(&argc,argv);
     std::cout << std::endl << "FreeGLUT version:" << std::endl;
     std::cout << glutGet(GLUT_VERSION);
     std::cout << std::endl;
+
+    /*glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100,100);
+	glutInitWindowSize(320,320);
+	glutCreateWindow("Lighthouse3D - GLUT Tutorial");
+
+	// register callbacks
+	glutDisplayFunc(renderScene);
+
+	// enter GLUT event processing cycle
+	glutMainLoop();*/
     return EXIT_SUCCESS;
 }
